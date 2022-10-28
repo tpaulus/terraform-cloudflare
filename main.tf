377f18163864e696cef54474c733ad6a5be6ac40
Tom Paulus <tom@tompaulus.com>
2022-10-27 21:42:07 -0700

    Add Gateway Policies

--- main.tf
+++ main.tf
@@ -219,3 +219,71 @@ resource "cloudflare_record" "whitestar_systems_brickyard_ips" {
 
 // ==== Zero Trust ====
 // TODO Access Policies
+
+
+// ==== Gateway Policies ====
+resource "cloudflare_teams_rule" "block_ads" {
+  name = "Block Ads"
+  description = ""
+  account_id  = local.cf_account_id
+  precedence = 10000
+  action = "block"
+  filters = ["dns"]
+  traffic = "any(dns.content_category[*] in {1})"  # https://developers.cloudflare.com/cloudflare-one/policies/filtering/dns-policies/dns-categories/#category-and-subcategory-ids
+  rule_settings {
+    block_page_enabled = false
+  }
+}
+
+resource "cloudflare_teams_rule" "block_content_categories" {
+  name = "Block Content Categories"
+  description = ""
+  account_id  = local.cf_account_id
+  precedence = 11000
+  action = "block"
+  filters = ["dns"]
+  traffic = "any(dns.content_category[*] in {85 162 128 161 177 169 124 170})"  # https://developers.cloudflare.com/cloudflare-one/policies/filtering/dns-policies/dns-categories/#category-and-subcategory-ids
+  rule_settings {
+    block_page_enabled = true
+  }
+}
+
+resource "cloudflare_teams_rule" "block_security_risks" {
+  name = "Block Security Risks"
+  description = ""
+  account_id  = local.cf_account_id
+  precedence = 12000
+  action = "block"
+  filters = ["dns"]
+  traffic = "any(dns.security_category[*] in {68 178 80 83 176 175 117 131 134 151 153})"  # https://developers.cloudflare.com/cloudflare-one/policies/filtering/dns-policies/dns-categories/#category-and-subcategory-ids
+  rule_settings {
+    block_page_enabled = true
+  }
+}
+
+resource "cloudflare_teams_rule" "block_manual_trackers_and_ads" {
+  name = "Block Manual Trackers & Ads"
+  description = ""
+  account_id  = local.cf_account_id
+  precedence = 13000
+  action = "block"
+  filters = ["dns"]
+  traffic = "any(dns.domains[*] in $534bce67f20a4ac698fc003f6c139767)"
+  rule_settings {
+    block_page_enabled = false
+  }
+}
+
+resource "cloudflare_teams_rule" "block_tor" {
+  name = "Block TOR"
+  description = ""
+  account_id  = local.cf_account_id
+  precedence = 14000
+  action = "block"
+  filters = ["dns"]
+  traffic = "any(dns.dst.geo.continent[*] == \"T1\")"
+  rule_settings {
+    block_page_enabled = true
+    block_page_reason = "Access to TOR Sites is blocked."
+  }
+}
