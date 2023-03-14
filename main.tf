a7729bf57b9f65304378ddb81f2a8487e981ac23
Tom Paulus <tom@tompaulus.com>
2023-03-14 15:40:50 -0700

    Revert "Add Immich as Nomad Service"
    This reverts commit 54080ea8858d8fed5728626481bc597cd05fedaa.

--- main.tf
+++ main.tf
@@ -139,7 +139,7 @@ resource "cloudflare_record" "whitestarsystems_com_keybase_verification" {
 locals {
   ws_n3d_fqdn = "n3d.brickyard.whitestar.systems"
 
-  ws_n3d_services = ["netbox", "m3d.brickyard",  "consul.brickyard", "nomad.brickyard", "grafana.brickyard", "prometheus.brickyard", "alertmanager.brickyard", "immich.brickyard"]
+  ws_n3d_services = ["netbox", "m3d.brickyard",  "consul.brickyard", "nomad.brickyard", "grafana.brickyard", "prometheus.brickyard", "alertmanager.brickyard"]
   ws_router_services = ["home", "woodlandpark-access.brickyard", "woodlandpark-smb.brickyard"]
 
   brickyard_local_ips = [
