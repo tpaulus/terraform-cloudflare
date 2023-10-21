448cac35aafdae8d27eb198946b4aec933bd0186
Tom Paulus <tom@tompaulus.com>
2023-10-21 12:01:02 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "4.17.0"
     }
+    nomad = {
+      source = "hashicorp/nomad"
+      version = "2.0.0"
+    }
   }
 }
 
@@ -11,4 +15,19 @@ terraform {
 provider "cloudflare" {
   email   = var.cloudflare_email
   api_key = var.cloudflare_api_key
+}
+
+
+provider "nomad" {
+  address = "https://nomad.brickyard.whitestar.systems"
+
+  headers {
+    name = "CF-Access-Client-Id"
+    value = var.cf_access_client_id
+  }
+
+  headers {
+    name = "CF-Access-Client-Secret"
+    value = var.cf_access_client_secret
+  }
 }
