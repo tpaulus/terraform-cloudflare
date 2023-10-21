b1ec0381227943de32d595b755f4887b02041dba
Tom Paulus <tom@tompaulus.com>
2023-10-21 15:01:42 -0700

    Configure Tunnel via API, not local file

--- tunnels.tf
+++ tunnels.tf
@@ -11,8 +11,20 @@ resource "cloudflare_tunnel" "brickyard_warp_tunnel" {
 resource "nomad_variable" "brickyard_warp_tunnel" {
   path  = "cloudflared/brickyard-warp"
   items = {
-    AccountTag = local.cf_account_id
-    TunnelID = cloudflare_tunnel.brickyard_warp_tunnel.id
-    TunnelSecret = random_id.brickyard_warp_tunnel_secret.b64_std
+    TunnelToken = cloudflare_tunnel.brickyard_warp_tunnel.tunnel_token
   }
+}
+
+resource "cloudflare_tunnel_virtual_network" "seaview-vnet" {
+  account_id = local.cf_account_id
+  name = "seaview"
+  comment = "Seaview Network"
+}
+
+resource "cloudflare_tunnel_route" "seaview_ip" {
+  account_id         = local.cf_account_id
+  tunnel_id          = cloudflare_tunnel.brickyard_warp_tunnel.id
+  network            = "10.0.0.0/8"
+  comment            = "Home Network Route"
+  virtual_network_id = cloudflare_tunnel_virtual_network.seaview_ip.id
 }
