38f50f1436e793f3c8d9147b417ba3b6df8cb1bd
Tom Paulus <tom@tompaulus.com>
2023-10-21 12:20:44 -0700

    Add Tunnels to TF and send secrets to Nomad (#42)
    * Update providers.tf
    
    * Update variables.tf
    
    * Update providers.tf
    
    * Create tunnels.tf
    
    * Update providers.tf
    
    * Update tunnels.tf

---
+++ tunnels.tf
@@ -0,0 +1,18 @@
+resource "random_id" "brickyard_warp_tunnel_secret" {
+  byte_length = 35
+}
+
+resource "cloudflare_tunnel" "brickyard_warp_tunnel" {
+  account_id = local.cf_account_id
+  name       = "brickyard-warp"
+  secret     = random_id.brickyard_warp_tunnel_secret.b64_std
+}
+
+resource "nomad_variable" "brickyard_warp_tunnel" {
+  path  = "cloudflared/brickyard-warp"
+  items = {
+    AccountTag = local.cf_account_id
+    TunnelID = cloudflare_tunnel.brickyard_warp_tunnel.id
+    TunnelSecret = cloudflare_tunnel.brickyard_warp_tunnel.tunnel_token
+  }
+}
