7405093ec62df0d8bb8279075e8b35b2d3b64bd3
Tom Paulus <tom@tompaulus.com>
2024-06-18 21:16:41 -0700

    chore: Add Records for PirateShip

--- paulus_family.tf
+++ paulus_family.tf
@@ -12,3 +12,18 @@ module "paulus_family_email" {
   create_wildcard_mx_records          = true
   dmarc_report_address                = "mailto:d6b6ea49728e40aabe6f5d3b65646b12@dmarc-reports.cloudflare.net"
 }
+
+resource "cloudflare_record" "pirate_ship_dkim" {
+  zone_id = cloudflare_zone.paulus_family.id
+  name    = "20240619041211pm._domainkey"
+  type    = "TXT"
+  value   = "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDNQxj6/H4/+X1gbz0khrP5c+LI7JMZNW/FC4laAJsuLThYh48ENFDH/6lW5MmjDdQcERbDYF6qm9bLmUjZzKkrXRQsPigf9+VSufKE4OU5QeT8zGZ/JdDKfHQLvIT6rqXgmPTd/7/SADQ6NSZSBN5NP30/z85EcEEJGzhD4FypVwIDAQAB"
+}
+
+resource "cloudflare_record" "pirate_ship_bounces" {
+  zone_id = cloudflare_zone.paulus_family.id
+  name    = "pm-bounces"
+  type    = "CNAME"
+  value   = "pm.mtasv.net"
+  proxied = false
+}
