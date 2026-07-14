53c36d823491df027fefa5f455d3a527e3595917
Tom Paulus <tom@tompaulus.com>
2026-07-13 19:30:19 -0700

    Remove Fastmail module from mel.earth Terraform config

--- mel_earth.tf
+++ mel_earth.tf
@@ -2,13 +2,3 @@ resource "cloudflare_zone" "mel_earth" {
   account_id = local.cf_account_id
   zone       = "mel.earth"
 }
-
-module "mel_earth_email" {
-  source = "./modules/fastmail"
-
-  zone_id                             = cloudflare_zone.mel_earth.id
-  fqdn                                = cloudflare_zone.mel_earth.zone
-  create_client_configuration_records = true
-
-  dmarc_report_address = "mailto:f36e4edc4151420abb491d4495fc879c@dmarc-reports.cloudflare.net"
-}
